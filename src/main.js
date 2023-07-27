deac27d17b86c193fa1123f7381700a8b922e288
CriSFV <cristina.f.valverde>
2023-07-27 18:07:43 +0200

    refactor: remove unuseful imports

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 import "./main.css";
-import "./app-element";
 import { Router } from "@vaadin/router";
 
 import "./pages/home.page";
-import "./pages/posts.page";
-
-console.log("Vanilla");
 
 const outlet = document.querySelector("#outlet");
 const router = new Router(outlet);
 
 router.setRoutes([
   { path: "/", component: "home-page" },
-  { path: "/posts", component: "posts-page" },
   { path: "(.*)", redirect: "/" },
 ]);
